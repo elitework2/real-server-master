1c91851187175896653a9cfe2ee68a4f78d4cd87
elitework2 <96829156+elitework2@users.noreply.github.com>
2022-01-03 15:25:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,111 @@ io.on('connection', (socket) => {
     io.emit('chat message', msg);
   });
 
+  var online = 0;
+var users = [];
+app.use(bodyParser.json());//parser utizado para requisições quando é via post, identificar o json no body, via get não há necessidade
+                            // os parametros vem no atributo query
+app.use(bodyParser.urlencoded({ extended : true }));
+
+var connection = mysql.createConnection({
+	"host": "localhost",
+	"user": "root",
+	"password": "",
+	"database": "messenger"
+});
+
+connection.connect(function (error) {
+	//
+});
+
+app.use(function (request, result, next) {
+	result.setHeader("Access-Control-Allow-Origin", "*");
+	next();
+});
+
+app.post("/get_messages", function (request, result) {
+	connection.query("SELECT * FROM user_chat WHERE (sender_id = '" + request.body.sender_id + "' AND receiver_id = '" + request.body.receiver_id + "') OR (sender_id = '" + request.body.receiver_id + "' AND receiver_id = '" + request.body.sender_id + "')", function (error, messages) {
+		result.end(JSON.stringify(messages));
+	});
+});
+
+
+
+io.on("connection", function (socket) {
+	console.log("User connected: ",  socket.id);
+
+	socket.on("user_connected", function (nome) {
+		users[nome] = socket.id;
+		io.emit("user_connected", nome);
+	});
+
+	socket.on("send_message", function (data) {
+		//var socketId = users[data.receiver_id];
+         users[data.receiver_id];
+    	io.emit("message_received", data);
+		//socket.to(socketId).emit("message_received", data);
+
+		//connection.query("INSERT INTO user_chat (sender_id, receiver_id, message) VALUES ('" + data.sender_id + "', '" + data.receiver_id + "', '" + data.message + "')", function (error, result) {
+			//
+		//});
+	});
+});
+
+
+app.get("/", function(req, res) {
+  //  res.sendFile(__dirname + '/index.php');
+    res.send("<h1>Ola mundo</h1>");
+})
+
+
+
+
+io.on('connection', (socket) => {
+
+online++;
+
+socket.on('newuser', () => {
+    io.emit('new', online);
+   
+    socket.on("user_connected", function (nome) {
+		users[nome] = socket.id;
+		io.emit("user_connected", nome);
+	});
+
+})
+
+socket.on('load', (data) => {
+    io.emit('load', data);
+   
+})
+
+socket.on('typing', (data) => {
+    io.emit('receiver_typing', data);
+   
+})
+
+socket.on('update_last_project', (data) => {
+    io.emit('update_last_project', data);
+   
+})
+
+socket.on('load_offer', (data) => {
+    io.emit('load_offer', data);
+   
+})
+
+socket.on('load_contact_msg', (data) => {
+    io.emit('load_contact_msg', data);
+   
+})
+
+//load_offer
+
+socket.on('disconect', () => {
+    online--;
+    io.emit('new', online)
+})
+  
 });
 
 let port = (process.env.PORT) ? process.env.PORT : 8000;
