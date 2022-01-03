0a4df4ff5901ddd646f32361d256cf43b03661cc
elitework2 <96829156+elitework2@users.noreply.github.com>
2022-01-03 14:58:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
 //app.use(bodyParser.urlencoded());
