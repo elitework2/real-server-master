ee7d6a1b8fbb2c5502958af05f8734770072311a
elitework2 <96829156+elitework2@users.noreply.github.com>
2022-01-03 15:35:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,53 +1,27 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const { Socket } = require('dgram');
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
 
 app.get('/', (req, res) => {
   //res.sendFile(__dirname + '/index.html');
   res.send("<h1>Ola mundo</h1>");
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-
-
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-
-  var online = 0;
+var online = 0;
 var users = [];
+
 app.use(bodyParser.json());//parser utizado para requisições quando é via post, identificar o json no body, via get não há necessidade
                             // os parametros vem no atributo query
 app.use(bodyParser.urlencoded({ extended : true }));
-
-var connection = mysql.createConnection({
-	"host": "localhost",
-	"user": "root",
-	"password": "",
-	"database": "messenger"
-});
-
-connection.connect(function (error) {
-	//
-});
 
 app.use(function (request, result, next) {
 	result.setHeader("Access-Control-Allow-Origin", "*");
 	next();
 });
-
-app.post("/get_messages", function (request, result) {
-	connection.query("SELECT * FROM user_chat WHERE (sender_id = '" + request.body.sender_id + "' AND receiver_id = '" + request.body.receiver_id + "') OR (sender_id = '" + request.body.receiver_id + "' AND receiver_id = '" + request.body.sender_id + "')", function (error, messages) {
-		result.end(JSON.stringify(messages));
-	});
-});
-
-
 
 io.on("connection", function (socket) {
 	console.log("User connected: ",  socket.id);
@@ -70,61 +44,72 @@ io.on("connection", function (socket) {
 });
 
 
-app.get("/", function(req, res) {
-  //  res.sendFile(__dirname + '/index.php');
-    res.send("<h1>Ola mundo</h1>");
-})
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+ 
+  
+});
 
 
 
 io.on('connection', (socket) => {
 
-online++;
-
-socket.on('newuser', () => {
-    io.emit('new', online);
-   
-    socket.on("user_connected", function (nome) {
-		users[nome] = socket.id;
-		io.emit("user_connected", nome);
-	});
-
-})
-
-socket.on('load', (data) => {
-    io.emit('load', data);
-   
-})
-
-socket.on('typing', (data) => {
-    io.emit('receiver_typing', data);
-   
-})
-
-socket.on('update_last_project', (data) => {
-    io.emit('update_last_project', data);
-   
-})
-
-socket.on('load_offer', (data) => {
-    io.emit('load_offer', data);
-   
-})
-
-socket.on('load_contact_msg', (data) => {
-    io.emit('load_contact_msg', data);
-   
-})
-
-//load_offer
-
-socket.on('disconect', () => {
-    online--;
-    io.emit('new', online)
-})
-  
-});
+    online++;
+    
+    socket.on('newuser', () => {
+        io.emit('new', online);
+       
+        socket.on("user_connected", function (nome) {
+            users[nome] = socket.id;
+            io.emit("user_connected", nome);
+        });
+    
+    })
+    
+    socket.on('load', (data) => {
+        io.emit('load', data);
+       
+    })
+    
+    socket.on('typing', (data) => {
+        io.emit('receiver_typing', data);
+       
+    })
+    
+    socket.on('update_last_project', (data) => {
+        io.emit('update_last_project', data);
+       
+    })
+    
+    socket.on('load_offer', (data) => {
+        io.emit('load_offer', data);
+       
+    })
+    
+    socket.on('load_contact_msg', (data) => {
+        io.emit('load_contact_msg', data);
+       
+    })
+    
+    //load_offer
+    
+    socket.on('disconect', () => {
+        online--;
+        io.emit('new', online)
+    })
+    
+    })
 
 let port = (process.env.PORT) ? process.env.PORT : 8000;
 
