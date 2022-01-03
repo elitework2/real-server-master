2678bce3bd2345a8a43fd17e78c2e73aea6d0d39
elitework2 <96829156+elitework2@users.noreply.github.com>
2022-01-03 15:45:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const bodyParser = require("body-parser");
-const mysql = require("mysql");
 
 app.get('/', (req, res) => {
   //res.sendFile(__dirname + '/index.html');
   res.send("<h1>Ola mundo</h1>");
 });
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
 
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+});
 
 let port = (process.env.PORT) ? process.env.PORT : 8000;
 
