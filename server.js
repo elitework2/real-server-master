be0c9b98c8a16afdb8910a42d3783bd5f11fe188
elitework2 <96829156+elitework2@users.noreply.github.com>
2022-01-03 15:59:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -40,56 +40,54 @@ io.on("connection", function (socket) {
   });
 
 
-io.on('connection', (socket) => {
+  io.on('connection', (socket) => {
 
-online++;
+    online++;
+    
+    socket.on('newuser', () => {
+        io.emit('new', online);
+       
+        socket.on("user_connected", function (nome) {
+            users[nome] = socket.id;
+            io.emit("user_connected", nome);
+        });
+    
+    })
+    
+    socket.on('load', (data) => {
+        io.emit('load', data);
+       
+    })
+    
+    socket.on('typing', (data) => {
+        io.emit('receiver_typing', data);
+       
+    })
+    
+    socket.on('update_last_project', (data) => {
+        io.emit('update_last_project', data);
+       
+    })
+    
+    socket.on('load_offer', (data) => {
+        io.emit('load_offer', data);
+       
+    })
+    
+    socket.on('load_contact_msg', (data) => {
+        io.emit('load_contact_msg', data);
+       
+    })
+    
+    //load_offer
+    
+    socket.on('disconect', () => {
+        online--;
+        io.emit('new', online)
+    })
+    
+    })
 
-socket.on('newuser', () => {
-    io.emit('new', online);
-   
-    socket.on("user_connected", function (nome) {
-		users[nome] = socket.id;
-		io.emit("user_connected", nome);
-	});
-
-})
-
-socket.on('load', (data) => {
-    io.emit('load', data);
-   
-})
-
-socket.on('typing', (data) => {
-    io.emit('receiver_typing', data);
-   
-})
-
-socket.on('update_last_project', (data) => {
-    io.emit('update_last_project', data);
-   
-})
-
-socket.on('load_offer', (data) => {
-    io.emit('load_offer', data);
-   
-})
-
-socket.on('load_contact_msg', (data) => {
-    io.emit('load_contact_msg', data);
-   
-})
-
-//load_offer
-
-socket.on('disconect', () => {
-    online--;
-    io.emit('new', online)
-})
-
-})
-
-
-});
 
 let port = (process.env.PORT) ? process.env.PORT : 8000;
 
