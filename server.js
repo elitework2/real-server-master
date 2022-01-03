07e6d5076c285e0a4cd044d9c0637daa27828dc5
elitework2 <96829156+elitework2@users.noreply.github.com>
2022-01-03 15:56:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,17 +13,81 @@ app.use(function (request, result, next) {
 	next();
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
 
 
+io.on("connection", function (socket) {
+	console.log("User connected: ",  socket.id);
+
+	socket.on("user_connected", function (nome) {
+		users[nome] = socket.id;
+		io.emit("user_connected", nome);
+	});
+
+	socket.on("send_message", function (data) {
+		//var socketId = users[data.receiver_id];
+         users[data.receiver_id];
+    	io.emit("message_received", data);
+		//socket.to(socketId).emit("message_received", data);
+
+		//connection.query("INSERT INTO user_chat (sender_id, receiver_id, message) VALUES ('" + data.sender_id + "', '" + data.receiver_id + "', '" + data.message + "')", function (error, result) {
+			//
+		//});
+	});
+});
+	
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
+
+
+io.on('connection', (socket) => {
+
+online++;
+
+socket.on('newuser', () => {
+    io.emit('new', online);
+   
+    socket.on("user_connected", function (nome) {
+		users[nome] = socket.id;
+		io.emit("user_connected", nome);
+	});
+
+})
+
+socket.on('load', (data) => {
+    io.emit('load', data);
+   
+})
+
+socket.on('typing', (data) => {
+    io.emit('receiver_typing', data);
+   
+})
+
+socket.on('update_last_project', (data) => {
+    io.emit('update_last_project', data);
+   
+})
+
+socket.on('load_offer', (data) => {
+    io.emit('load_offer', data);
+   
+})
+
+socket.on('load_contact_msg', (data) => {
+    io.emit('load_contact_msg', data);
+   
+})
+
+//load_offer
+
+socket.on('disconect', () => {
+    online--;
+    io.emit('new', online)
+})
+
+})
+
 
 });
 
